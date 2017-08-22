03ba4eeade27ea6cf915cc591b607f7e82bc548c
lbq <lbq@LEO-MBP.local>
2017-08-22 11:07:54 +0800

    创建工程

---
+++ DMM3U8Download/DMM3U8Download/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DMM3U8Download
+//
+//  Created by lbq on 2017/8/22.
+//  Copyright © 2017年 lbq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DMM3U8Download/DMM3U8Download/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DMM3U8Download
+//
+//  Created by lbq on 2017/8/22.
+//  Copyright © 2017年 lbq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
